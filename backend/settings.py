f95be1c06e9f22b322c4c5b1c2fdc2966494b023
Sukruthi <sukruthisantosh@gmail.com>
2025-06-30 10:42:02 +0100

    feat: adds allowed host

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u=g3xriascl^%b*=#9cit0@r_ji86ls&!gn&y-p@@-=xpfd92o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pdf-backend-vc88.onrender.com']
 
 
 # Application definition
