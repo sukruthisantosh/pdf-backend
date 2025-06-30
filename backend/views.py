09722e0b74ebedea8ec20d8cf1d8be34bbd1ff80
Sukruthi <sukruthisantosh@gmail.com>
2025-06-30 11:05:01 +0100

    feat: changes settings

--- backend/views.py
+++ backend/views.py
@@ -1,4 +1,6 @@
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
 
+@api_view(["GET"])
 def hello(request):
     return JsonResponse({"message": "Hello from Django!"})
